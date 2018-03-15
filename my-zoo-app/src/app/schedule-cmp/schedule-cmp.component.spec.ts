ef9626f89e456e0897c2f63177c09ad4d4fed616
Spencer Saunders <svs1@math.sc.edu>
2018-03-15 08:18:43 -0400

    Update (hardcoded) schedule

--- my-zoo-app/src/app/schedule-cmp/schedule-cmp.component.spec.ts
+++ my-zoo-app/src/app/schedule-cmp/schedule-cmp.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {DebugElement } from '@angular/core';
 import { ScheduleCmpComponent } from './schedule-cmp.component';
+import { By } from '@angular/platform-browser';
 
 describe('ScheduleCmpComponent', () => {
   let component: ScheduleCmpComponent;
   let fixture: ComponentFixture<ScheduleCmpComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,4 +25,9 @@ describe('ScheduleCmpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+//   it('should display each element of the list in the table', () =>{
+//     var listWhat = fixture.debugElement.query(By.css('.what'));
+//     var listEl = listWhat.nativeElement;
+//     expect(listEl.textContent).toEqual('Breakfast with Orangutan');
+// })
 });
--- my-zoo-app/src/app/schedule-cmp/schedule-cmp.component.ts
+++ my-zoo-app/src/app/schedule-cmp/schedule-cmp.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleCmpComponent implements OnInit {
 
+
+  schedule: {}[] = [
+    {what:"Breakfast with Orangutan", where:"Kampung Sumatra", when:"8:00 AM - 9:30 AM"},
+    {what:"Elephants Bathing", where:"Elephant Pool", when:"8:45 AM - 9:00 AM"},
+    {what:"Short Animal Presenation", where:"Gaya Restaurant", when:"9:00 AM - 9:15 AM"},
+    {what:"Animal Presentation", where:"Kampung Sumatra Stage", when:"10:00 AM - 10:20 AM"}, 
+    {what:"The Exotica Wildlife Education", where:"Green Stage", when:"11:15 AM - 11:45 AM"},
+    {what:"Tiger Keeper Talk", where:"Tiger Exhibit", when:"11:45 AM - 12:00 PM"},
+    {what:"Lemur Keeper Talk", where:"Aviary", when:"12:15 PM - 12:30 PM"},
+    {what:"Animal Encounter", where:"Elephant View Restaurant", when:"1:00 PM - 2:00 PM"}
+    ];
+
   constructor() { }
 
   ngOnInit() {
