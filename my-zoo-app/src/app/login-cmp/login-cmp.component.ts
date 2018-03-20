d15cda923a06b95b2155ccee3f8135c29cf71aec
jsoria53 <soriajose53@gmail.com>
2018-03-20 00:15:33 -0400

    added some more styles

--- my-zoo-app/src/app/login-cmp/login-cmp.component.ts
+++ my-zoo-app/src/app/login-cmp/login-cmp.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
-<<<<<<< HEAD
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import * as jwtDecode from "jwt-decode";
-=======
-//import { Http, Response } from '@angular/http';
->>>>>>> 031c90bbe7f83ee8c443b80345cb5faa697bd30b
 
 @Component({
   selector: 'app-login-cmp',
@@ -19,44 +15,39 @@ export class LoginCmpComponent implements OnInit {
   password: string;
 
  
-  constructor(private router: Router) { }
+  constructor(private router: Router,private http: Http) { }
 
   ngOnInit() {
   }
 
-  // myUserLogin(event){
-  //   var body = "uname"+this.username+
-  // }
-  
-  // myClickManager(event){
-  //   this.http.get('http://localhost:8080/auth?username='+this.username).
-  //     map(
-  //       (response) => response.text()
-  //     ).
-  //     subscribe(
-  //       (data) => {console.log(data)}
-  //     )
-   
-  //   }
 
 
   onClickSubmit(event){
+
     var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJtYW5hZ2VyIiwic3ViIjoiS2ltIiwiaXNzIjoiWm9vdHJvcG9saXMiLCJleHAiOjE1MjIzNTA2NTl9.kWpv_TUTxkVZZoZKFMegaNFI9q4zR64vP-4pSLfQVGFkqzZr97r5EJJ8zqEvRNJ_hG-n2aUocAkVwAfQ_8Ltcw'
-    var decoded = jwtDecode(token);
-    console.log(decoded);
+    var data2 = jwtDecode(token);
+    console.log(data2);
+    localStorage.setItem("Username",data2['sub']);
+    localStorage.setItem("Password",data2['iss']);
 
 
-    // var body = { "username":"Kim","password":"lol"}
+    // var body = { "username":this.username,"password":this.password}
 
     // console.log(body)
-    // this.http.put('https://zootropolis.herokuapp.com/users/signin',body).
+    // this.http.post('https://zootropolis.herokuapp.com/users/signin',body).
     // map (
     //   (response) => response.text()
     // ).subscribe (
     //   (data) => {console.log(data)}
     // )
-    // this.router.navigate(['app-administrator-cmp'])
+   this.router.navigate(['app-administrator-cmp'])
   }
+
+  // submitData(data){
+    // var token = data;
+    // var data2 = jwtDecode(token);
+    // console.log(data2);
+  // }
 
 
 }
