c0e4e4eb7d7e563d17892480b17724519e3d944a
jsoria53 <soriajose53@gmail.com>
2018-03-24 22:50:44 -0400

    added top five functionality

--- my-zoo-app/src/app/bar-cmp/bar-cmp.component.ts
+++ my-zoo-app/src/app/bar-cmp/bar-cmp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-bar-cmp',
@@ -6,13 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-cmp.component.css']
 })
 export class BarCmpComponent implements OnInit {
+    stuff: {}[] = [];
+    data;
+    httpdata;
+    data2;
   width = 600;
     height = 400;
     type = 'column2d';
     dataFormat = 'json';
     dataSource;
 
-  constructor() {
+  constructor(private http: Http) {
+    const headers = new Headers();
+    headers.append('Content-Type', "application/json");
+    headers.append('Authorization', 'Bearer'+ ' '+localStorage.getItem("token"));
+    const options = new RequestOptions({headers: headers});
+    var body = ' '
+    this.http.post('https://zootropolis.herokuapp.com/Animal/topfive',body,options).
+    map (
+      (response) => response.text()
+    ).subscribe (
+      (data) => {this.display(data)}
+    )
+
+
+
     this.dataSource = this.dataSource = {
       "chart": {
           "caption": "Animal Popularity",
@@ -20,30 +39,24 @@ export class BarCmpComponent implements OnInit {
         //   "numberprefix": "$",
           "theme": "fint"
       },
-      "data": [
-          {
-              "label": "Lion",
-              "value": "10"
-          },
-          {
-              "label": "Panda",
-              "value": "5"
-          },
-          {
-              "label": "American Bison",
-              "value": "3"
-          },
-          {
-              "label": "Alligators",
-              "value": "9"
-          },
-          {
-              "label": "Apes",
-              "value": "20"
-          }
-      ]
+      "data": this.stuff
   }
    }
+
+   display(data){
+    
+    var i = 0;
+    this.httpdata = JSON.parse(data)
+    console.log(this.httpdata)
+   for (var key in this.httpdata) {
+         if (this.httpdata.hasOwnProperty(key)) {
+       var val = this.httpdata[key];
+       this.data ={label: val["animalName"], value: val["tracking"].toString()};
+       this.stuff.push(this.data)
+     }
+    
+   }
+}
 
   ngOnInit() {
   }
